038d28d64fc6e4b64be89dad94d779115127a782
fatemanazari <fatemanazari78@gmail.com>
2023-10-11 20:23:23 +0330

    Update schema file

--- schema.sql
+++ schema.sql
@@ -2,8 +2,8 @@
 
 CREATE TABLE IF NOT EXISTS public.animals
 (
-    id integer NOT NULL,
-    name character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(100) COLLATE pg_catalog."default",
     date_of_birth date,
     escape_attempts integer,
     neutered boolean,
