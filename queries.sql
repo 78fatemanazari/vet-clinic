00e293ccc656b4f76ae9157c633f44e060d509da
fatemanazari <fatemanazari78@gmail.com>
2023-10-11 20:21:24 +0330

    Add queries to the tables

--- queries.sql
+++ queries.sql
@@ -1,3 +1,18 @@
-/*Queries that provide answers to the questions from all projects.*/
+/* Queries about animals table*/
 
-SELECT * from animals WHERE name = 'Luna';
+/* The 1st query */
+SELECT * From animals Where name Like '%mon';
+/* The 2nd query */
+SELECT id, name From animals Where (date_of_birth > '2015-12-31' and date_of_birth < '2020-01-01');
+/* The 3rd query */
+SELECT id, name From animals Where (neutered = true and escape_attempts < '3');
+/* The 4th query*/
+SELECT id, name, date_of_birth from animals Where name in ('Agumon', 'Pikachu');
+/* The 5th query*/
+SELECT id, name, escape_attempts from animals Where (weight_kg > '10.5');
+/* The 6th query */
+SELECT * From animals Where neutered = true ; 
+/* The 7th qeury*/
+SELECT * From animals Where name not in ('Gabumon'); 
+/* The 8th query*/
+SELECT * From animals Where (weight_kg >= '10.4' and weight_kg <= '17.3');
