97c867b4b55672f9a4b724b7b8cc3449099f1ee8
fatemanazari <fatemanazari78@gmail.com>
2023-10-13 01:36:36 +0330

    Add schema, data and queries

--- data.sql
+++ data.sql
@@ -17,3 +17,48 @@ values ('Charmander', '2020-02-08', '0', false, '-11'),
 ('Blossom', '1998-10-13', '3', true, '17'),
 ('Ditto', '2022-05-14', '4', true, '22');
 
+--Third project
+
+--Inser data into owners table
+INSERT INTO owners (full_name, age)
+VALUES ('Sam Smith', '34'),
+('Jennifer Orwell', '19'),
+('Bob', '45'),
+('Melody Pond', '77'),
+('Dean Winchester', '14'),
+('Jodie Whittaker', '38');
+
+--Insert data into species table
+INSERT INTO species (name)
+VALUES ('Pokemon'),
+('Digimon');
+
+--Modify your inserted animals so it includes the species_id value
+UPDATE animals
+SET species_id = (
+    CASE 
+        WHEN name LIKE '%mon' THEN (SELECT id FROM species WHERE name = 'Digimon')
+        ELSE (SELECT id FROM species WHERE name = 'Pokemon')
+    END
+);
+
+--Modify your inserted animals to include owner information (owner_id)
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Sam Smith')
+WHERE name = 'Agumon';
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Jennifer Orwell')
+WHERE name IN ('Gabumon', 'Pikachu');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Bob')
+WHERE name IN ('Devimon', 'Plantmon');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Melody Pond')
+WHERE name IN ('Charmander', 'Squirtle', 'Blossom');
+
+UPDATE animals
+SET owner_id = (SELECT id FROM owners WHERE full_name = 'Dean Winchester')
+WHERE name IN ('Angemon', 'Boarmon');
--- schema.sql
+++ schema.sql
@@ -16,3 +16,25 @@ CREATE TABLE IF NOT EXISTS public.animals
 ALTER TABLE IF EXISTS public.animals
     ADD COLUMN IF NOT EXISTS species character varying(100) COLLATE pg_catalog."default";
 
+--Third project
+
+CREATE TABLE IF NOT EXISTS public.animals
+(
+    id integer NOT NULL GENERATED ALWAYS AS IDENTITY ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 2147483647 CACHE 1 ),
+    name character varying(100) COLLATE pg_catalog."default",
+    date_of_birth date,
+    escape_attempts integer,
+    neutered boolean,
+    weight_kg real,
+    species_id integer,
+    owner_id integer,
+    CONSTRAINT animals_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id)
+        REFERENCES public.owners (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION,
+    CONSTRAINT fk_species FOREIGN KEY (species_id)
+        REFERENCES public.species (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+)
